9030cdc657fae0301b34139591b8cfe645feb7ae
Barbosa <nicolas.a.barbosa@accenture.com>
2019-08-16 16:57:25 -0300

    adding base methods to category-form component

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoryListComponent } from './category-list/category-list.component';
@@ -11,7 +12,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   imports: [
     CommonModule,
     CategoriesRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    ReactiveFormsModule
   ]
 })
 export class CategoriesModule { }
--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -1,19 +1,83 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
 import { faAngleDoubleLeft, faSave } from '@fortawesome/free-solid-svg-icons';
+import { CategoryService } from '../shared/category.service';
+import { Category } from '../shared/category.model';
+import { switchMap } from 'rxjs/operators';
+import toastr from 'toastr';
 
 @Component({
   selector: 'app-category-form',
   templateUrl: './category-form.component.html',
   styleUrls: ['./category-form.component.css']
 })
-export class CategoryFormComponent implements OnInit {
+export class CategoryFormComponent implements OnInit, AfterContentChecked {
 
   public faAngleDoubleLeft = faAngleDoubleLeft;
   public faSave = faSave;
+  public currencAction: string;
+  public categoryForm: FormGroup;
+  public pageTitle: string;
+  public serverErrorMessages: string[] = null;
+  public submittingForm = false;
+  public category: Category = new Category();
 
-  constructor() { }
+  constructor(
+    private categoryService: CategoryService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.setCurrentaction();
+    this.buildCategoryForm();
+    this.loadCategory();
+  }
+
+  ngAfterContentChecked(): void {
+    this.setPageTitle();
+  }
+
+  // PRIVATE METHODS
+
+  private setCurrentaction(): void {
+    this.route.snapshot.url[0].path === 'new' ? this.currencAction = 'new' : this.currencAction = 'edit';
+  }
+
+  private buildCategoryForm(): void {
+    this.categoryForm = this.formBuilder.group({
+      id: [null],
+      name: [null, [Validators.required, Validators.minLength(2)]],
+      description: [null]
+    });
+  }
+
+  private loadCategory(): void {
+    if (this.currencAction === 'edit') {
+      this.route.paramMap
+      .pipe(
+        switchMap(params => this.categoryService.getById(+params.get('id')))
+      )
+      .subscribe({
+        next: category => {
+          this.category = category;
+          this.categoryForm.patchValue(this.category);
+        },
+        error: _ => alert('Ocorreu um erro no servidor')
+      });
+    }
+  }
+
+  private setPageTitle(): void {
+    if (this.currencAction === 'new') {
+      this.pageTitle = 'Cadastro de nova Categoria';
+    } else {
+      const categoryName = this.category.name || '...';
+      this.pageTitle = `Editando a categoria: ${categoryName}`;
+    }
   }
 
 }

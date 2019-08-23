a9083d17ed66badc00a8f70a612f61633f063473
nicolas.a.barbosa <nicolas.a.barbosa@accenture.com>
2019-08-23 11:48:19 -0300

    adding ReportsComponent && Routing

--- src/app/pages/reports/reports.module.ts
+++ src/app/pages/reports/reports.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { ReportsRoutingModule } from './reports-routing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { ReportsComponent } from './reports/reports.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ReportsComponent],
   imports: [
-    CommonModule,
+    SharedModule,
     ReportsRoutingModule
   ]
 })
